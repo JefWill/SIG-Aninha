43d45ac8c7c14b7c5874fdbbea2eb9bffa233934
JefWill <jeffersonwillamern@gmail.com>
2025-08-13 22:15:32 -0300

    Estrutura inicial do Codigo
    foi adicionado a estrutura base das funções, e a assinatura das funções

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
-int main(int argc, char const *argv[])
-{
-    printf("ola mundo");
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Assinatura das funções
+void tela_menu_principal(void);
+void tela_sobre(void);
+void tela_equipe(void);
+
+void tela_menu_cliente(void);
+void tela_menu_agendamento(void);
+void tela_menu_signos(void);
+void tela_menu_tarot(void);
+void tela_menu_relatorio(void);
+
+int main(void) {
+    tela_menu_principal();
+    tela_sobre();
+    tela_equipe();
+
+    tela_menu_cliente();
+    tela_menu_agendamento();
+    tela_menu_signos();
+    tela_menu_tarot();
+    tela_menu_relatorio();
+    
     return 0;
 }
