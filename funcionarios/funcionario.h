c6776e3d8783e392b76b80e6487cd4ea37b0589c
Vinicius Linhares <vinicius.linhares.702@ufrn.edu.br>
2025-10-03 00:03:43 -0300

    feat: cria struct funcionario

--- funcionarios/funcionario.h
+++ funcionarios/funcionario.h
@@ -1,4 +1,14 @@
 /////  FUNCIONARIO  //////
+typedef struct funcionario Funcionario;
+
+struct funcionario
+{
+    char cpf[15];
+    char nome[50];
+    char cargo[50];
+    char cpf_lido[15];
+};
+
 int tela_menu_funcionario(void);
 void modulo_funcionario(void);
 void cadastrar_funcionario(void);
